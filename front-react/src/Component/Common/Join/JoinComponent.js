69ae636efd9c175ac7f95eaa08e65a2a3d87afa1
lnvel <lnvel0203@gmail.com>
2023-04-20 22:56:06 +0900

    로그아웃쫌 바꿈

--- front-react/src/Component/Common/Join/JoinComponent.js
+++ front-react/src/Component/Common/Join/JoinComponent.js
@@ -99,6 +99,29 @@ class JoinComponent extends Component {
             />
           </div>
 
+           <div className="text_area">
+            <input
+              type="text"
+              id="birth"
+              name="birth"
+              value={this.state.email}
+              className="text_input"
+              placeholder="생년월일을 입력하세요"
+              onChange={this.onChange}
+            />
+          </div>
+
+          <div className="text_area">
+            <input
+              type="birth"
+              id="birth"
+              name="birth"
+              value={this.state.email}
+              className="text_input"
+              placeholder="아아아 를 입력하세요"
+              onChange={this.onChange}
+            />
+          </div>
           <button onClick={this.signOn} className="btn">
             Sign On
           </button>
